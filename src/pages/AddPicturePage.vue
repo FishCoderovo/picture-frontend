3606320098d85f5a3254ccd4c7f489b911293c41
hsy <991279557@qq.com>
2025-02-12 22:20:54 +0800

    图片模块0

--- src/pages/AddPicturePage.vue
+++ src/pages/AddPicturePage.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="addPicturePage">
     <h1>{{ msg }}</h1>
+    <PictureUpload/>
   </div>
 </template>
 
 <script setup lang="ts">
+import PictureUpload from '@/components/PictureUpload.vue'
+
 const msg = "欢迎";
 </script>
 
